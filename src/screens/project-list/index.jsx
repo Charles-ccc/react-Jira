eff71a9576348721f04bbb641adb0db0a86c0f71
刘畅 <charlessliu@tencent.com>
2021-10-10 14:20:31 +0800

    feat: 添加自定义hook

--- src/screens/project-list/index.jsx
+++ src/screens/project-list/index.jsx
@@ -1,7 +1,7 @@
 import React, {useState, useEffect} from 'react'
 import { SearchPanel } from "./search-panel"
 import { List } from "./list"
-import { cleanObject } from 'utils'
+import { cleanObject, useDebounce } from 'utils'
 import qs from 'qs'
 
 const apiUrl = process.env.REACT_APP_API_URL
@@ -13,14 +13,14 @@ export const ProjectListScreen = () => {
   })
   const [users, setUsers] = useState([])
   const [list, setList] = useState([])
-
+  const debouncedParam = useDebounce(param, 2000)
   useEffect(() => {
-    fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(param))}`).then(async res => {
+    fetch(`${apiUrl}/projects${qs.stringify(cleanObject(debouncedParam))}`).then(async res => {
       if (res.ok) {
         setList(await res.json())
       }
     })
-  }, [param])
+  }, [debouncedParam])
 
   useEffect(() => {
     fetch(`${apiUrl}/users`).then(async res => {
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 export const isFalsy = (value) => (value === 0 ? false : !value);
 
 export const cleanObject = (object) => {
@@ -9,4 +10,34 @@ export const cleanObject = (object) => {
     }
   });
   return result;
+};
+
+export const useMount = (callback) => {
+  useEffect(() => {
+    callback();
+  }, []);
+};
+
+export const debounce = (func, delay) => {
+  let timer;
+  return (...param) => {
+    if (timer) {
+      clearTimeout(timer);
+    }
+    timer = setTimeout(function () {
+      func(...param);
+    }, delay);
+  };
+};
+
+export const useDebounce = (value, delay) => {
+  const [debounceValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    // 每次在value变化以后，设置一个定时器
+    const timer = setTimeout(() => setDebouncedValue(value), delay);
+    // 每次在上一个useEffect处理完以后再运行
+    return () => clearTimeout(timer);
+  }, [value, delay]);
+  return debounceValue;
 };
