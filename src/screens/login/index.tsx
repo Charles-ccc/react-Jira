723acf233ea1a8dbd82413c3d945993502408c82
刘畅 <charlessliu@tencent.com>
2021-10-14 15:37:38 +0800

    feature: login

---
+++ src/screens/login/index.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+
+interface ILogin {
+  username: string;
+  password: string;
+}
+const apiUrl = process.env.REACT_APP_API_URL;
+
+export const LoginScreen = () => {
+  const login = (param: ILogin) => {
+    fetch(`${apiUrl}/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(param),
+    }).then(async (res) => {
+      if (res.ok) {
+      }
+    });
+  };
+
+  // FormEventHandler<HTMLFormElement>
+  const handleSubmit = (event: React.SyntheticEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const username = (event.currentTarget.elements[0] as HTMLFormElement).value;
+    const password = (event.currentTarget.elements[1] as HTMLFormElement).value;
+    login({ username, password });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="username">用户名</label>
+        <input type="text" id={"username"} />
+      </div>
+      <div>
+        <label htmlFor="password">密码</label>
+        <input type="text" id={"password"} />
+      </div>
+      <button type={"submit"}>登录</button>
+    </form>
+  );
+};
