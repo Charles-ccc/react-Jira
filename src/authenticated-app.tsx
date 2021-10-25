43a1bedc80bedf37c4758847fb33f525c8b1f662
刘畅 <charlessliu@tencent.com>
2021-10-25 16:52:06 +0800

    feat: 用flex和grid布局

--- src/authenticated-app.tsx
+++ src/authenticated-app.tsx
@@ -2,20 +2,68 @@
 import { ProjectListScreen } from "screens/project-list";
 import { useAuth } from "context/auth-context";
 import styled from "@emotion/styled";
+
+/**
+ * grid 和 flex 的应用场景
+ * 1. 要考虑一维布局（flex)还是二维布局(grid)
+ * 2. 是从内容出发（flex）还是布局出发（grid）
+ */
 export const AuthenticatedApp = () => {
   const { logout } = useAuth();
   return (
     <Container>
-      <header>
-        <button onClick={logout}>登出</button>
-      </header>
-      <main>
+      <Header>
+        <HeaderLeft>
+          <h3>Logo</h3>
+          <h3>项目</h3>
+          <h3>用户</h3>
+        </HeaderLeft>
+        <HeaderRight>
+          <button onClick={logout}>登出</button>
+        </HeaderRight>
+      </Header>
+      <Nav>nav</Nav>
+      <Main>
         <ProjectListScreen />
-      </main>
+      </Main>
+      <Aside>aside</Aside>
+      <Footer>footer</Footer>
     </Container>
   );
 };
 const Container = styled.div`
   display: grid;
-  grid-template-rows: 6rem calc(100vh - 6rem);
+  grid-template-rows: 6rem 1fr 6rem;
+  grid-template-columns: 20rem 1fr 20rem;
+  grid-template-areas:
+    "header header header"
+    "nav main aside"
+    "footer footer footer";
+  height: 100vh;
+  grid-gap: 10rem;
+`;
+// grid-area 用来给grid子元素起名字
+const Header = styled.header`
+  grid-area: header;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
+const HeaderLeft = styled.div`
+  display: flex;
+  align-items: center;
+`;
+const HeaderRight = styled.div``;
+const Main = styled.main`
+  grid-area: main;
+`;
+const Nav = styled.nav`
+  grid-area: nav;
+`;
+const Aside = styled.aside`
+  grid-area: aside;
+`;
+const Footer = styled.footer`
+  grid-area: footer;
 `;
