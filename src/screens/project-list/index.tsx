ff873a46b75853d8a7c036d97a0bc61bf0dad371
刘畅 <charlessliu@tencent.com>
2021-10-26 15:58:19 +0800

    feat:  封装useAsync等自定义hook

--- src/screens/project-list/index.tsx
+++ src/screens/project-list/index.tsx
@@ -1,46 +1,28 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { SearchPanel } from "./search-panel";
 import { List } from "./list";
-import { cleanObject, useDebounce } from "utils";
-import { useHttp } from "utils/http";
+import { useDebounce } from "utils";
+import { useUsers } from "utils/user";
+import { useProjects } from "utils/project";
 import styled from "@emotion/styled";
 import { Typography } from "antd";
-
 export const ProjectListScreen = () => {
   const [param, setParam] = useState({
     name: "",
     personId: "",
   });
-  const [users, setUsers] = useState([]);
-  const [list, setList] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState<null | Error>(null);
-  const client = useHttp();
-  const debouncedParam = useDebounce(param, 1000);
-
-  useEffect(() => {
-    setIsLoading(true);
-    client("projects", { data: cleanObject(debouncedParam) })
-      .then(setList)
-      .catch((error) => {
-        setList([]);
-        setError(error);
-      })
-      .finally(() => {
-        setIsLoading(false);
-      });
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [debouncedParam]);
-
-  useEffect(() => {
-    client("users").then(setUsers);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
+  const debouncedParam = useDebounce(param, 500);
+  const { isLoading, error, data: list } = useProjects(debouncedParam);
+  const { data: users } = useUsers();
   const panelProps = {
     param,
     setParam,
-    users,
+    users: users || [],
+  };
+  const lisetProps = {
+    loading: isLoading,
+    dataSource: list || [],
+    users: users || [],
   };
   return (
     <Container>
@@ -49,7 +31,7 @@ export const ProjectListScreen = () => {
       {error ? (
         <Typography.Text type="danger">{error.message}</Typography.Text>
       ) : null}
-      <List loading={isLoading} dataSource={list} users={users} />
+      <List {...lisetProps} />
     </Container>
   );
 };
